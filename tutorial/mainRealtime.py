23d08f91f6968875d0dbfa650a9778acbf5b09b0
Nguyễn Đức Lộc <85562445+NgDucLoc@users.noreply.github.com>
2023-12-07 17:04:38 +0700

    .
    fix some bugs

--- tutorial/mainRealtime.py
+++ tutorial/mainRealtime.py
@@ -5,16 +5,22 @@ from tutorial.spiders.GetPriceRealtime import PriceRealtime
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import time
+
+from multiprocessing import Process  # Sửa đổi dòng import
+
+
 def mainRealtime():
     setting = get_project_settings()
     process = CrawlerProcess(setting)
     process.crawl(PriceRealtime)
     process.start()
 
-    import sys
-    del sys.modules['twisted.internet.reactor']
+    # import sys
+    # del sys.modules['twisted.internet.reactor']
 
 if __name__ == "__main__":
     while(True):
-        mainRealtime()
+        p = Process(target=mainRealtime)
+        p.start()
+        p.join()
         time.sleep(60)
